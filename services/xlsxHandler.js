5d04f82004fa22edda400c21c71bca2c46547066
ppbra <kaka020998@gmail.com>
2018-12-18 13:43:34 +0700

    a

--- services/xlsxHandler.js
+++ services/xlsxHandler.js
@@ -42,7 +42,7 @@ module.exports = {
                             if (!students[R - 10])
                                 students[R - 10] = {};
 
-                            students[R - 10][headers[C]] = worksheet[XLSX.utils.encode_cell(cell_addr)].v;
+                            students[R - 10][headers[C]] = worksheet[XLSX.utils.encode_cell(cell_addr)].v.trim();
                         }
                     } else break;
 
@@ -92,7 +92,7 @@ module.exports = {
                             if (!teachers[R - 1])
                                 teachers[R - 1] = {};
 
-                            teachers[R - 1][headers[i]] = worksheet[XLSX.utils.encode_cell({ c: i, r: R })].v
+                            teachers[R - 1][headers[i]] = worksheet[XLSX.utils.encode_cell({ c: i, r: R })].v.trim()
                         }
                     } else {
                         break;
@@ -129,7 +129,7 @@ module.exports = {
                             if (!students[R - 1])
                                 students[R - 1] = {};
 
-                            students[R - 1][headers[i]] = worksheet[XLSX.utils.encode_cell({ c: i, r: R })].v
+                            students[R - 1][headers[i]] = worksheet[XLSX.utils.encode_cell({ c: i, r: R })].v.trim();
                         }
                     } else {
                         break;
